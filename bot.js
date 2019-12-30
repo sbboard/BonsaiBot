64cb75186f7f29f5c276bd51c7bcaa5ae80b752d
sbboard <colin.buffum@gmail.com>
2019-12-29 23:43:13 -0700

    hubeproofed

--- bot.js
+++ bot.js
@@ -4,10 +4,20 @@ const client = new Discord.Client();
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
-    
-  if (msg.content.toLowerCase().includes('bonsai')) {
-    msg.reply('BONSAI!');
+})
+client.on('message', msg => {
+  if (msg.content.toLowerCase().includes('bonsai') && msg.author.username != "BonsaiBro") {
+      
+    const channel = client.channels.find('name', msg.channel.name)
+    //msg.reply('BONSAI!!!!');
+    if(msg.author.username.toLowerCase() == "hube"){        
+        channel.send('Ted Hubish is a fraud')
+    }
+    else{
+        channel.send('BONSAI!!')
+    }
   }
-});
+  console.log(msg)
+})
 
 client.login(secrets.token);
