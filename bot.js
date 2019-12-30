01b3c94ed060f5b3bd761438a94784020d5db4e3
sbboard <colin.buffum@gmail.com>
2019-12-29 23:23:15 -0700

    I think i did it

--- bot.js
+++ bot.js
@@ -4,6 +4,10 @@ const client = new Discord.Client();
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
+    
+  if (strpos(msg.content.toLowerCase()), 'bonsai') {
+    msg.reply('BONSAI!');
+  }
 });
 
 client.login(secrets.token);
