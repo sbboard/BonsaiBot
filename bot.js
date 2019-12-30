883f56aa3e6dd3274213e6d5bfa2cd61a727dbf1
sbboard <colin.buffum@gmail.com>
2019-12-29 23:16:59 -0700

    initial connection w/ discord made

---
+++ bot.js
@@ -0,0 +1,9 @@
+const secrets = require("./secrets.js")
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.login(secrets.token);
